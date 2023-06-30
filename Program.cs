db58d5d19fba1de338ff541081d2295beed18a21
Niapa <niapa7@yandex.ru>
2023-06-30 19:46:41 +0700

    1-3 commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,26 @@
 ﻿/*
+
+
+
+
+
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int Point(int b1, int b2, int k1, int k2)
+
+int size = InputNum("Введите b1: ");
+int size = InputNum("Введите k1: ");
+int size = InputNum("Введите b2: ");
+int size = InputNum("Введите k2: ");
+double  x = -(b1-b2)/(k1-k2);
+double  y = k1*x + b1;
+
+
 Задача 41: 
 __________
 Пользователь вводит с клавиатуры M чисел. 
@@ -7,6 +29,47 @@ __________
 1, -7, 567, 89, 223-> 3
 ________________________________________________________________________
 
+
+int Input(string message)
+{
+   Console.WriteLine(message);
+   return int.Parse(Console.ReadLine()!);
+}
+
+int[] InputSomeNums()
+{
+    int[] i nputArray = new int[Input()]
+
+}
+
+
+
+string PrintArray(int[] array)
+{
+   string res = String.Empty;
+   for (int i = 0; i < array.Length; i++)
+   {
+      res += array[i] + " ";
+   }
+   return res;
+}
+
+void Count(int[] array)
+{
+    int i = 0;
+    int res = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0) res += res;
+    return res;
+}       
+  
+int m = Input("Введите число M: ");
+int[] myArray = Input("Введите массив чисел: ");
+InputSomeNums();
+Count(myArray);
+Console.WriteLine(PrintArray(res));
+
+*/
 Задача 43:
 __________
 Напишите программу, которая найдёт точку пересечения двух прямых, 
@@ -15,4 +78,46 @@ __________
 _________________________________________________________________________
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-*/
+
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[] CreateArray(int size)
+{
+    return new int[size];
+}
+
+void FillArray(int[] array)
+{
+    Random rnd = new Random(); 
+    for (int i = 0; i < array.Length; i++)
+        array[i] = rnd.Next(); 
+}
+
+string PrintArray(int[] array)
+{
+    string res = String.Empty;
+    for (int i = 0; i < array.Length; i++)
+        res += array[i] + " ";
+    return res;
+}
+
+int Sum(int [] array)
+{
+    int res = 0;
+    for (int i = 0; i < array.Length; i+=2)
+    res += array[i];
+    return num;
+}
+int size = InputNum("Введите размер массива: ");
+int [] myArray = CreateArray(size);
+FillArray(myArray);
+string res = PrintArray(myArray);
+Console.WriteLine(res);
+int num = Sum(myArray);
+Console.WriteLine($"Сумма элементов на нечетных позициях равна: {num}.");
+
